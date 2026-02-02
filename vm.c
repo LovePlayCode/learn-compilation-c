96c163ab61ca3f7f5a595eea14f7359af40f0de7
LovePlayCode <1244224501@qq.com>
2026-02-02 11:27:33 +0800

    feat: 重构编译器，添加解析器和错误处理功能，更新编译函数以支持 Chunk

--- vm.c
+++ vm.c
@@ -6,43 +6,49 @@
 
 VM vm;
 
-static void resetStack() {
+static void resetStack()
+{
     vm.stackTop = vm.stack;
 }
 
-void initVM() {
+void initVM()
+{
     resetStack();
-
 }
 
-Value pop() {
+Value pop()
+{
     vm.stackTop--;
     return *vm.stackTop;
 }
 
-void push(Value value) {
+void push(Value value)
+{
     *vm.stackTop = value;
     vm.stackTop++;
 }
 
-void freeVM() {
+void freeVM()
+{
 }
 
 /**
  * 任何指令的第一个字节都是操作码。给定一个操作码，我们需要找到实现该指令语义的正确的 C 代码，这个过程被称为解码或指令分派。
  ***/
-static InterpretResult run() {
+static InterpretResult run()
+{
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op) \
-do { \
-double b = pop(); \
-double a = pop(); \
-push(a op b); \
-} while (false)
+#define BINARY_OP(op)     \
+    do                    \
+    {                     \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b);     \
+    } while (false)
 
-    for (;;) {
-        
+    for (;;)
+    {
 
 /**
  * chunk->code (数组起始地址)
@@ -60,7 +66,8 @@ vm.ip = 当前执行到的指令地址
  */
 #ifdef DEBUG_TRACE_EXECUTION
         printf("          ");
-        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+        {
             printf("[ ");
             printValue(*slot);
             printf(" ]");
@@ -70,32 +77,59 @@ vm.ip = 当前执行到的指令地址
                                (int)(vm.ip - vm.chunk->code));
 #endif
         uint8_t instruction;
-        switch (instruction = READ_BYTE()) {
-            case OP_CONSTANT: {
-                Value constant = READ_CONSTANT();
-                push(constant);
-                break;
-            }
-            case OP_ADD:      BINARY_OP(+); break;
-            case OP_SUBTRACT: BINARY_OP(-); break;
-            case OP_MULTIPLY: BINARY_OP(*); break;
-            case OP_DIVIDE:   BINARY_OP(/); break;
-            case OP_NEGATE:   push(-pop()); break;
-            case OP_RETURN: {
-                printValue(pop());
-                printf("\n");
-                return INTERPRET_OK;
-            }
+        switch (instruction = READ_BYTE())
+        {
+        case OP_CONSTANT:
+        {
+            Value constant = READ_CONSTANT();
+            push(constant);
+            break;
+        }
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
+        case OP_NEGATE:
+            push(-pop());
+            break;
+        case OP_RETURN:
+        {
+            printValue(pop());
+            printf("\n");
+            return INTERPRET_OK;
+        }
         }
     }
 
 #undef READ_BYTE
 #undef READ_CONSTANT
 #undef BINARY_OP
-
 }
 
-InterpretResult interpret(const char* source) {
-    compile(source);
-    return INTERPRET_OK;
+InterpretResult interpret(const char *source)
+{
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk))
+    {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
